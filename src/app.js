c7cfd5a2c6fe62a269229fce9687d06d40502e0f
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-07-05 16:15:20 +0200

    add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const todoRouter = require("./routers/todos.router");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+  })
+);
 
 app.use(express.json()); // to accept JSON body in POST request
 
